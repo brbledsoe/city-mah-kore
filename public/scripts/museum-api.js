a5114dc0539b8c340770b8d3874eb1deb5ddc31d
eap47 <lordrepton@gmail.com>
2017-06-15 20:26:41 +0300

    THERES A MAP

--- public/scripts/museum-api.js
+++ public/scripts/museum-api.js
@@ -1,1 +1,25 @@
 //elios file
+function initMap() {
+	//map options
+    var options = {
+        zoom: 14,
+        center: { lat: 32.0853, lng: 34.7818 }
+    }
+    //new map
+    var map = new google.maps.Map(document.getElementById('map'), options);
+
+    //add marker
+    var marker= new google.maps.Marker({
+    	position:{lat:32.0777, lng: 34.7867},
+    	map:map,
+    	icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+    });
+
+    var infoWindow = new google.maps.InfoWindow({
+    	content:'<h1>TLV Museum of Art</h1>'
+    });
+
+    marker.addListener('click', function(){
+    	infoWindow.open(map, marker);
+    }); 
+}
