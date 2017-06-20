a678ae3152dec992ad030531c086c9f845567633
eap47 <lordrepton@gmail.com>
2017-06-20 14:36:41 +0300

    6/20 update
    split page, added dropdown

--- public/scripts/museum-api.js
+++ public/scripts/museum-api.js
@@ -1,25 +1,25 @@
 //elios file
 function initMap() {
-	//map options
+    //map options
     var options = {
-        zoom: 14,
-        center: { lat: 32.0853, lng: 34.7818 }
-    }
-    //new map
+            zoom: 14,
+            center: { lat: 32.0853, lng: 34.7818 }
+        }
+        //new map
     var map = new google.maps.Map(document.getElementById('map'), options);
 
     //add marker
-    var marker= new google.maps.Marker({
-    	position:{lat:32.0777, lng: 34.7867},
-    	map:map,
-    	icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+    var marker = new google.maps.Marker({
+        position: { lat: 32.0777, lng: 34.7867 },
+        map: map,
+        icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
     });
 
     var infoWindow = new google.maps.InfoWindow({
-    	content:'<h1>TLV Museum of Art</h1>'
+        content: '<h1>TLV Museum of Art | מוזיאון לאמנות תל אביב</h1>'
     });
 
-    marker.addListener('click', function(){
-    	infoWindow.open(map, marker);
-    }); 
+    marker.addListener('click', function() {
+        infoWindow.open(map, marker);
+    });
 }
